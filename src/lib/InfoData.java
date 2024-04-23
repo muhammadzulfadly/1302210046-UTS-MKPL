bd522f2e747ccb15e28bef758c5f89bea0055769
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2024-04-24 06:32:15 +0700

    menambahkan class InfoData dan mengurangi parameter Employee

---
+++ src/lib/InfoData.java
@@ -0,0 +1,47 @@
+package lib;
+
+public class InfoData {
+    private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+
+    public InfoData(String firstName, String lastName, String idNumber, String address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = idNumber;
+		this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
